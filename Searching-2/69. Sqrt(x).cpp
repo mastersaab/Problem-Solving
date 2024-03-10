ba3e19f7a3d85acb8ce61010244eac317df202da
Utkarsh Singh <98454843+mastersaab@users.noreply.github.com>
2024-03-10 08:31:55 +0530

    Create 69. Sqrt(x).cpp

---
+++ Searching-2/69. Sqrt(x).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        int l=1;
+        int h=x;
+        int ans=0;
+        while(l<=h){
+           int  mid=l+(h-l)/2;
+            if(mid <=x/mid){
+                ans= mid;
+                l=mid+1;
+            } 
+            //else if(mid<x/mid) l=mid+1;
+            else h=mid-1;
+        }
+        return ans;
+    }
+};
