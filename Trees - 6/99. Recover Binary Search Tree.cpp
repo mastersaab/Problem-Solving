374f32e26fd05e678436cfcd2e735c1350065cfd
Utkarsh Singh <98454843+mastersaab@users.noreply.github.com>
2024-04-01 22:12:03 +0530

    Update 99. Recover Binary Search Tree.cpp

--- Trees - 6/99. Recover Binary Search Tree.cpp
+++ Trees - 6/99. Recover Binary Search Tree.cpp
@@ -1,3 +1,32 @@
+APPROACH -1->
+    
+void inorder(TreeNode* root,TreeNode*& prev,TreeNode*& first,TreeNode*& end)
+    {
+        if(root == NULL) return;
+        
+            inorder(root->left,prev,first,end);
+            if(prev){
+                if(root->val<prev->val){
+                    if (!first) {
+                        first = prev;
+                    }
+                    end=root;
+                }
+            } 
+            prev=root;
+            inorder(root->right,prev,first,end);
+        
+    }
+    void recoverTree(TreeNode* root) {
+       TreeNode* prev=NULL,*first=NULL,*end=NULL;
+       inorder(root,prev,first,end);
+       swap(first->val,end->val); 
+       return;
+    }    
+
+
+APPROACH -2->
+    
 class Solution {
     vector<int> v{};
     void inorder(TreeNode* root)
