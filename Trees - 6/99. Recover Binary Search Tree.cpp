dfb66be107b4ddf22e0972dc5c518e6656073c61
Utkarsh Singh <98454843+mastersaab@users.noreply.github.com>
2024-04-01 20:30:05 +0530

    Update 99. Recover Binary Search Tree.cpp

--- Trees - 6/99. Recover Binary Search Tree.cpp
+++ Trees - 6/99. Recover Binary Search Tree.cpp
@@ -25,7 +25,7 @@ class Solution {
 public:
     void recoverTree(TreeNode* root) {
        inorder(root);
-       sort(v.begin(), v.end(), [](int &a, int &b){return a > b;});
+       sort(v.begin(), v.end(),);
        check(root);
 
     }
